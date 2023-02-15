60a042851736b7a98e51326951799de49ed3ed40
UroojFatima1 <uroojfatima1518@gmail.com>
2023-02-15 20:28:22 +0500

    first commit

---
+++ Assets/Scripts/Color_script.cs
@@ -0,0 +1,59 @@
+
+using UnityEngine;
+
+public class Color_script : MonoBehaviour
+{public LayerMask Raycast_layer;
+    public Camera Cam;
+    public Color[] colorList;
+    public Color curColor;
+    public int colorCount;
+    string spritename;
+    AudioManager audiomanager;
+    GameObject [] Instances;
+    
+
+    private void Start()
+    {
+        audiomanager = FindObjectOfType<AudioManager>();
+        
+    }
+    void Update()
+    {
+        curColor = colorList[colorCount];
+        
+        var ray = Cam.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray, -Vector2.up);
+        //GameObject hand = GameObject.Find("clicking");
+       
+
+
+        if (hit.collider != null)
+        {
+            Instances = GameObject.FindGameObjectsWithTag(hit.collider.name);
+            foreach (GameObject g in Instances) {
+                SpriteRenderer sp = g.GetComponent<SpriteRenderer>();
+                curColor.a = 1f;
+                sp.color = curColor;
+            }
+        }
+            
+               
+                
+            }
+
+
+    public void paint(int colorCode) {
+        colorCount = colorCode;
+        if (colorCode == 1) {
+            audiomanager.Play("Red");
+        }
+        if (colorCode == 2)
+        {
+            audiomanager.Play("Blue");
+        }
+        if (colorCode == 3)
+        {
+            audiomanager.Play("Yellow");
+        }
+    }
+}
